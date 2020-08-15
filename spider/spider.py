e04587d7b5f44864a4a6b6498aa63f376e4fa307
asterisk <fanguofan@tiaozhan.com>
2020-08-16 00:17:39 +0800

    make table

--- spider/spider.py
+++ spider/spider.py
@@ -26,7 +26,7 @@ if driver.current_url == "https://org.xjtu.edu.cn/openplatform/login.html":
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[text()='成绩查询']"))).click()
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[text()='进入服务']"))).click()
 
-time.sleep(1)
+time.sleep(0.5)
 wins = driver.window_handles
 driver.switch_to.window(wins[-1])
 
@@ -40,7 +40,6 @@ WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[t
 
 count_text = driver.find_element_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//span[@class='bh-pager-num']").get_attribute("textContent")
 count = result1 = re.findall(r'\d+', count_text)[2]
-print(count)
 
 driver.find_element_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//div[contains(@id, 'dropdownlistWrapper')]").click()
 # driver.find_element_by_xpath("//div[contains(@id, 'listBoxContentinnerListBox')]/div").scrollTop=100000
@@ -50,29 +49,44 @@ workbook = xlwt.Workbook(encoding = 'utf-8')
 worksheet = workbook.add_sheet('Grade')
 
 for page in range(int(int(count)/10)+1):
-  # 删了就炸
-  normal_data = driver.find_elements_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//tr")
-  
+
   size = int(count) - page * 10 if (page + 1) * 10 >= int(count) else 10
-  print(size)
-  for i in range(size):
-    
+  time.sleep(1)
+  for i in range(size):  
     cell = driver.find_elements_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//tr")[i].find_elements_by_xpath("./td/span")
+    grade = driver.find_elements_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//tr")[i].find_element_by_xpath("./td[@class='jqx-cell jqx-grid-cell jqx-item jqx-center-align']")
+    last_line = 0
+    for j in range(len(cell)):
+      worksheet.write(i + page * 10 + 1, j, label = cell[j].get_attribute("textContent"))
+      last_line = j
+    last_line += 1  
+    worksheet.write(i + page * 10 + 1, last_line , label = grade.get_attribute("textContent"))
+
+
     link = driver.find_elements_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//tr")[i].find_element_by_xpath("./td/a").click()
     time.sleep(0.2)
 
     rank_data = driver.find_elements_by_xpath("//div[@class='bh-mt-16 h3  jw-fontWeight-normal']")
     rank_text = rank_data[0].find_element_by_xpath("./span[@class='pm-bfb']").get_attribute("textContent")
-    print(rank_text)
+    last_line += 1
+    worksheet.write(i + page * 10 + 1, last_line , label = rank_text)
+
     class_grade_list = rank_data[2].find_elements_by_xpath("./span")
     for j in range(len(class_grade_list)):
       class_grade_text = class_grade_list[j].find_elements_by_xpath("./span")[2].get_attribute("textContent")
-      print(class_grade_text)
+      last_line += 1
+      worksheet.write(i + page * 10 + 1, last_line , label = class_grade_text)
+
     rank_group = driver.find_elements_by_xpath("//div[@class='bh-col-md-4 ']")
     rank_pjf_text = rank_group[0].find_element_by_xpath(".//span[@id='cjfb-pjf']").get_attribute("textContent")
     rank_zdf_text = rank_group[1].find_element_by_xpath(".//span[@id='cjfb-zdf']").get_attribute("textContent")
     rank_zgf_text = rank_group[2].find_element_by_xpath(".//span[@id='cjfb-zgf']").get_attribute("textContent")
-    print(rank_pjf_text, rank_zdf_text, rank_zgf_text)
+    last_line += 1
+    worksheet.write(i + page * 10 + 1, last_line , label = rank_pjf_text)
+    last_line += 1
+    worksheet.write(i + page * 10 + 1, last_line , label = rank_zdf_text)
+    last_line += 1
+    worksheet.write(i + page * 10 + 1, last_line , label = rank_zgf_text)
 
     # 课程排名有bug暂时搁置
     # driver.find_element_by_xpath("//div[@id='dropdownlistContentcjcx-rank-container']").click()
@@ -88,15 +102,9 @@ for page in range(int(int(count)/10)+1):
     
     driver.find_element_by_xpath("//div[@class='bh-property-dialog-container bh-animated bh-card bh-card-lv2 bh-intoRight']/i").click()
     time.sleep(0.2)
-
-
-    
-    # for j in range(len(cell)):
-      # worksheet.write(i,j, label = cell[j].get_attribute("textContent"))
-
   driver.find_elements_by_xpath("//div[@class='cjcx-tab-content-1 bh-mt-8 jqx-tabs-content-element jqx-rc-b']//i[@class='iconfont icon-keyboardarrowright']/..")[1].click()
 # 保存
-# workbook.save('Grade.xls')
+workbook.save('Grade.xls')
 
 time.sleep(10)
 driver.quit()
