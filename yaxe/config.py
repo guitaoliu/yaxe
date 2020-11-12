e03f13535e1390cfac8a1ba99295afec3512f935
Guitao Liu <guitao.liu@Outlook.com>
2020-11-12 22:23:52 +0800

    refract: reconstruct the project

---
+++ yaxe/config.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+
+from rich import print
+
+config_example = {"WEB": {"PUBLIC_KEY": "0725@pwdorgopenp"}, "USER": {}}
+CONFIG_FILE = "config.json"
+
+
+def load_config() -> None:
+    config_file = Path(CONFIG_FILE)
+    if not config_file.exists():
+        print("No config file detected using default config instead.")
+        print("please input your netid: ", end="")
+        netid = input().strip()
+        print("Please input your password: ", end="")
+        password = input().strip()
+        config_example["USER"]["NETID"] = netid
+        config_example["USER"]["PASSWORD"] = password
+        with open(CONFIG_FILE, "w", encoding="utf8") as f:
+            json.dump(config_example, f)
+        return config_example
+    else:
+        return json.loads(config_file.read_text())
+
+
+class Config:
+    def __init__(self) -> None:
+        config = load_config()
+
+        self.net_id = config["USER"]["NETID"]
+        self.password = config["USER"]["PASSWORD"]
+        self.public_key = config["WEB"]["PUBLIC_KEY"]
+
+
+config = Config()
--- yaxe/utils.py
+++ yaxe/utils.py
@@ -2,7 +2,8 @@ import base64
 import time
 
 from Crypto.Cipher import AES
-from config import config
+
+from yaxe.config import config
 
 
 def encrypt_passward(password: str) -> str:
@@ -14,13 +15,10 @@ def encrypt_passward(password: str) -> str:
         padding = chr(amount_to_pad)
         return text + padding * amount_to_pad
 
-    cipher = AES.new(
-        config.public_key.encode('utf-8'),
-        AES.MODE_ECB
-    )
+    cipher = AES.new(config.public_key.encode("utf-8"), AES.MODE_ECB)
 
-    encrypted_data = cipher.encrypt(pad(password).encode('utf-8'))
-    return str(base64.b64encode(encrypted_data), encoding='utf-8')
+    encrypted_data = cipher.encrypt(pad(password).encode("utf-8"))
+    return str(base64.b64encode(encrypted_data), encoding="utf-8")
 
 
 def get_timestamp() -> int:
